62ab5ad29ecad3c24c325fc8e934863e9697a04f
Raymart Berena <rberena@rrc.ca>
2025-09-02 10:44:55 -0500

    Completed first part of JavaScript Tutorial

---
+++ OneDrive/Documents/Javascript Training/chapter 2/sandbox.js
@@ -0,0 +1,22 @@
+// Camel-case is used for variables
+let myAge = 25;
+let year = 2025;
+
+// log things to the console
+console.log(age, year);
+
+/*
+This 
+is 
+a 
+multi-line 
+comment
+*/ 
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var score = 75;
+console.log(score);
