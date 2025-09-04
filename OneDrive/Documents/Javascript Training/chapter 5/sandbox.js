d2a19abcfdb6c47b2ab51c8445953821615e4a3c
Raymart Berena <rberena@academic.rrc.ca>
2025-09-04 13:19:04 -0500

    Added code for Object Literal section of JavaScript Training

--- OneDrive/Documents/Javascript Training/chapter 5/sandbox.js
+++ OneDrive/Documents/Javascript Training/chapter 5/sandbox.js
@@ -4,3 +4,25 @@ Date: September 4, 2025
 */
 
 // OBJECTS
+
+// Object Literals
+let user = {
+    name: 'Raymart',
+    age: 28,
+    email: 'rberena@academic.rrc.ca',
+    location: 'Winnipeg',
+    teams: ['Winnipeg Jets', 'Boston Celtics']
+};
+
+console.log(user.email);
+
+user.email = "rberena@rrc.ca"; // Change property 
+
+console.log(user.email);
+
+// Using square brackets accomplishes the same task
+user[0] = "Raymart Berena"; // Can also use "user['name']""
+
+console.log(user[0]);
+
+console.log(typeof(user));
