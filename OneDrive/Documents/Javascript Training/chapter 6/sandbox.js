669ebf41552a29a3ac975bd83168aa1bef3e4a26
Raymart Berena <rberena@academic.rrc.ca>
2025-09-04 15:16:53 -0500

    Added code for querying DOM for elements in JavaScript tutorial

--- OneDrive/Documents/Javascript Training/chapter 6/sandbox.js
+++ OneDrive/Documents/Javascript Training/chapter 6/sandbox.js
@@ -2,3 +2,28 @@
 Author: Raymart Berena
 Date: September 4, 2025
 */
+
+// DOM - Document Object Model
+// const para = document.querySelector("p");
+// const error = document.querySelector(".error"); // p tag with a class of error
+const divError = document.querySelector("div.error"); // div tag with class of error
+// console.log(para);
+console.log(divError);
+
+// const errors = document.querySelectorAll(".error")
+
+// console.log(errors);
+
+// Get an element by ID
+const title = document.getElementById("page-title");
+
+console.log(title);
+
+// Get element by class name
+const errors = document.getElementsByClassName("error");
+
+console.log(errors);
+
+// Get elements by tag name
+const paras = document.getElementsByTagName("p");
+console.log(paras[2]);
